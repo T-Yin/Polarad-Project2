3403811c5d221f2365101f24360b62165b657a80
T-Yin <xtiffany.yin@gmail.com>
2019-03-04 14:30:12 -0700

    add handlebars, some images, some js

---
+++ routes/polaradcontroller.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+
+  res.render("index");
+});
+
+router.get("/upload", function (req, res) {
+
+  res.render("upload");
+});
+
+router.get("/update-profile", function (req, res) {
+
+  res.render("edit");
+});
+
+module.exports = router;
