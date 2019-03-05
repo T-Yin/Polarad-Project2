b116dc88465277ef9f21cc47e3fe24883bc5da01
Tanner Galloway <tannergalloway15@gmail.com>
2019-03-05 15:14:39 -0700

    fixed account page

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
 // add hover effect to like button
-function hover(element) {
-    $(element).attr("src", "/images/heart-outline-color.png");
-  }
+function hover(element) 
+{
+  $(element).attr("src", "/images/heart-outline-color.png");
+}
   
-  function unhover(element) {
-    $(element).attr("src", "/images/heart-outline.png");
-  }
+function unhover(element) 
+{
+  $(element).attr("src", "/images/heart-outline.png");
+}
